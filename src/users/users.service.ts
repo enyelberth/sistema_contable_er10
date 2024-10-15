084003e8b660743ec5b944794cf5f49d54c68491
enyelberth <enyelberthrc22.z@gmail.com>
2024-10-15 15:42:48 -0400

    feat

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,45 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  prisma: any;
+
+  constructor() {
+    this.prisma = new PrismaClient();
+  }
+  async create(createUserDto: CreateUserDto) {
+    const user = await this.prisma.Usuarios.findMany();
+    return {
+      status: 200,
+      data: user,
+    };
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll(id: number) {
+    const user = await this.prisma.Usuarios.findMany({
+      where: {
+        id: id,
+      },
+    });
+
+    return {
+      status: 200,
+      data: user,
+    };
   }
 
-  findOne(id: number) {
+  async findOne(id: number) {
     return `This action returns a #${id} user`;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
+  async update(id: number, updateUserDto: UpdateUserDto) {
     return `This action updates a #${id} user`;
   }
 
-  remove(id: number) {
+  async remove(id: number) {
     return `This action removes a #${id} user`;
   }
 }
